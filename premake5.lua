9236f2ec754a35bfd21a965aba113f91e70811f9
Michael Mathu <michaelmathu6@gmail.com>
2024-07-18 16:44:16 +0300

    first commit

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/glad/glad.h",
+		"src/glad.c"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
